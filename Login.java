6067eac3e94dda9ee2efea73dfbf65f77aec3ad2
uotavia <uotavia@gmail.com>
2025-09-25 19:26:36 +0700

    login, menu, dan main

---
+++ Login.java
@@ -0,0 +1,26 @@
+package kuis1_pbo_kelompok2;
+
+import java.util.ArrayList;
+
+public class Login {
+
+    public Peserta loginPeserta(ArrayList<Peserta> daftar, String username, String password) {
+        for (int i = 0; i < daftar.size(); i++) {
+            Peserta pes = daftar.get(i);
+            if (pes.getUsername().equals(username) && pes.getPassword().equals(password)) {
+                return pes;
+            }
+        }
+        return null;
+    }
+
+    public Instruktur loginInstruktur(ArrayList<Instruktur> daftar, String username, String password) {
+    for (int i = 0; i < daftar.size(); i++) {
+        Instruktur ins = daftar.get(i); 
+        if (ins.getUsername().equals(username) && ins.getPassword().equals(password)) {
+            return ins;
+        }
+    }
+    return null;
+}
+}
--- Main.java
+++ Main.java
@@ -1,22 +1,45 @@
 package kuis1_pbo_kelompok2;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        
+        ArrayList<Peserta> daftarPeserta = new ArrayList<>();
+        ArrayList<Instruktur> daftarInstruktur = new ArrayList<>();
 
-        Peserta peserta1 = new Peserta("peserta01", "1234", "Marsya", "Malang");
-        peserta1.infoPeserta();
-        peserta1.lihatKonten("Video");
-        peserta1.lihatKonten("Artikel");
-        peserta1.lihatKonten("Kuis");
-        peserta1.pembayaran("Materi Java OOP");
+        daftarPeserta.add(new Peserta("peserta1", "123", "Budi", "Jakarta"));
+        daftarPeserta.add(new Peserta("peserta2", "abc", "Siti", "Bandung"));
 
-        System.out.println("===================================");
+        daftarInstruktur.add(new Instruktur("instruktur1", "456", "Pak Andi", "Java"));
+        daftarInstruktur.add(new Instruktur("instruktur2", "xyz", "Bu Rina", "Python"));
+        Login login = new Login();
+        Scanner scanner = new Scanner(System.in);
 
-        Instruktur instruktur1 = new Instruktur("instruktur01", "abcd", "Pak Roy", "Pemrograman");
-        instruktur1.infoInstruktur();
-        instruktur1.tambahKonten("Video Polymorphism");
-        instruktur1.tambahMateri("Jobsheet 4.4 - Inheritance");
-        instruktur1.lihatKonten();
-        instruktur1.lihatMateri();
+        System.out.println("=== Sistem Login ===");
+        System.out.print("Username: ");
+        String username = scanner.nextLine();
+        System.out.print("Password: ");
+        String password = scanner.nextLine();
+
+        Peserta pesertaLogin = login.loginPeserta(daftarPeserta, username, password);
+        if (pesertaLogin != null) {
+            System.out.println("\nLogin berhasil sebagai Peserta.");
+            Menu menu = new Menu();
+            menu.menuPeserta(pesertaLogin);
+            return;
+        }
+
+        Instruktur instrukturLogin = login.loginInstruktur(daftarInstruktur, username, password);
+        if (instrukturLogin != null) {
+            System.out.println("\nLogin berhasil sebagai Instruktur.");
+            Menu menu = new Menu();
+            menu.menuInstruktur(instrukturLogin);
+            return;
+        }
+
+        System.out.println("\nLogin gagal. Username atau password salah.");
+
     }
 }
